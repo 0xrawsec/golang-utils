731aea0712dc04ac6a6627dd1844ab89f7295e34
Quentin JEROME <qjerome@users.noreply.github.com>
2017-09-06 23:52:01 +0200

    Changed SyncedMap: Added Get/Set/Keys/Values method (not tested :() and modified Contains

--- datastructs/syncedmap.go
+++ datastructs/syncedmap.go
@@ -24,11 +24,44 @@ func (s *SyncedMap) Del(key interface{}) {
 	delete(s.m, key)
 }
 
-func (s *SyncedMap) Contains(key interface{}) (value interface{}, ok bool) {
+func (s *SyncedMap) Contains(key interface{}) (ok bool) {
+	s.RLock()
+	defer s.RUnlock()
+	_, ok = s.m[key]
+	return
+}
+
+func (s *SyncedMap) Get(key interface{}) (value interface{}, ok bool) {
 	s.RLock()
 	defer s.RUnlock()
 	value, ok = s.m[key]
 	return
+}
+
+func (s *SyncedMap) Keys() chan interface{} {
+	ci := make(chan interface{})
+	go func() {
+		defer close(ci)
+		s.RLock()
+		defer s.RUnlock()
+		for k, _ := range s.m {
+			ci <- k
+		}
+	}()
+	return ci
+}
+
+func (s *SyncedMap) Values() chan interface{} {
+	ci := make(chan interface{})
+	go func() {
+		defer close(ci)
+		s.RLock()
+		defer s.RUnlock()
+		for _, v := range s.m {
+			ci <- v
+		}
+	}()
+	return ci
 }
 
 func (s *SyncedMap) Len() int {
